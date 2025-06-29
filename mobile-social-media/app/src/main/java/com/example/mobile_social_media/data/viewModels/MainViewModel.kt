91cde40898fe3bf11a339f8c4018ed8783ee3a54
matheusGuedes0 <matheusrsguedes1@gmail.com>
2025-06-29 13:27:13 -0300

    feat(viewModels): add view models and add firebase configs and dependencies

---
+++ mobile-social-media/app/src/main/java/com/example/mobile_social_media/data/viewModels/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.mobile_social_media.data.viewModels
+import com.example.mobile_social_media.data.model.Usuario
+import com.example.mobile_social_media.data.repository.UsuarioRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+
+class MainViewModel(
+    private val usuarioRepository: UsuarioRepository = UsuarioRepository()
+) : ViewModel() {
+
+    private val _usuario = MutableStateFlow<Usuario?>(null)
+    val usuario: StateFlow<Usuario?> = _usuario
+
+    fun carregarUsuarioAtual() {
+        val usuarioLocal = usuarioRepository.getUsuarioAtual()
+        if (usuarioLocal != null) {
+            viewModelScope.launch {
+                usuarioRepository.salvarUsuario(usuarioLocal)
+                _usuario.value = usuarioLocal
+            }
+        }
+    }
+}
